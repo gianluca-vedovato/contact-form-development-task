ebca91b0b88183b554aa2a3b45067d162535ec95
gianlucavedovato-akqa <gianluca.vedovato@akqa.com>
2025-07-10 23:46:05 +0200

    feat: basic text component

---
+++ src/components/BasicText.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p :class="$style.basicText"><slot /></p>
+</template>
+
+<style module lang="scss">
+.basicText {
+  font-size: var(--text-m);
+  line-height: 1.428;
+  color: var(--color-text-50);
+  a {
+    color: var(--color-primary-100);
+    text-decoration: underline;
+    transition: opacity 0.2s ease-in-out;
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+}
+</style>

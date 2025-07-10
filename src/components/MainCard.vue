9c79e03950e31aba7cb3573672ee64b58b4dc9fe
gianlucavedovato-akqa <gianluca.vedovato@akqa.com>
2025-07-10 23:46:14 +0200

    feat: main card component

---
+++ src/components/MainCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div :class="$style.card">
+    <slot />
+  </div>
+</template>
+
+<style module lang="scss">
+@use '@/assets/styles/mixins';
+
+.card {
+  background-color: var(--color-primary-50);
+  border-radius: var(--rounded);
+  padding: var(--space-xl);
+  width: 100%;
+
+  @include mixins.mq(md) {
+    width: 500px;
+  }
+}
+</style>
